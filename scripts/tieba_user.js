1ef71bb7f5fde6040c1968820e686adcdb5a6332
Newflav <149877783+Newflav@users.noreply.github.com>
2023-12-23 06:35:05 +0800

    Add files via upload

---
+++ scripts/tieba_user.js
@@ -0,0 +1,8 @@
+var body = $response.body; var json = JSON.parse(body);
+// 过滤掉特定关键词的对象 if (json.hasOwnProperty("vip_banner")) { delete json["vip_banner"]; }
+if (json.hasOwnProperty("finance_tab")) { delete json["finance_tab"]; }
+if (json.hasOwnProperty("recom_swan_list")) { delete json["recom_swan_list"]; } if (json.hasOwnProperty("namoaixud_entry")) { delete json["namoaixud_entry"]; } if (json.hasOwnProperty("duxiaoman_entry")) { delete json["duxiaoman_entry"]; } if (json.hasOwnProperty("banner")) { delete json["banner"]; } if (json.hasOwnProperty("recom_naws_list")) { delete json["recom_naws_list"]; }
+// 过滤掉"user"中的"user_growth" if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("user_growth")) { delete json["user"]["user_growth"]; }
+if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("ip_address")) { delete json["user"]["ip_address"]; }
+if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("gift_list")) { delete json["user"]["gift_list"]; }
+$done({ body: JSON.stringify(json) });
