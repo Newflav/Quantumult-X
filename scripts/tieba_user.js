0ffa480a913462c6d17c53dde1ff60c7498444f8
Newflav <149877783+Newflav@users.noreply.github.com>
2023-12-23 07:37:45 +0800

    Update tieba_user.js

--- scripts/tieba_user.js
+++ scripts/tieba_user.js
@@ -17,11 +17,20 @@ delete json["banner"];
 if (json.hasOwnProperty("recom_naws_list")) {
 delete json["recom_naws_list"];
 }
+if (json.hasOwnProperty("recom_swan_list")) {
+delete json["recom_swan_list"];
+}
+if (json.hasOwnProperty("finance_tab")) {
+delete json["finance_tab"];
+}
 
 // 过滤掉"user"中的"user_growth"
 if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("user_growth")) {
 delete json["user"]["user_growth"];
 }
 if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("ip_address")) { delete json["user"]["ip_address"]; }
+if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("gift_list")) { delete json["user"]["gift_list"]; }
+if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("shake_ad_switch")) { delete json["user"]["shake_ad_switch"]; }
+if (json.hasOwnProperty("user") && json["user"].hasOwnProperty("virtual_image_info")) { delete json["user"]["virtual_image_info"]; }
 
 $done({ body: JSON.stringify(json) });
