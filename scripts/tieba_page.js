d44398d47d609225884172b04fbb765dd6fc016f
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-02 23:58:34 +0800

    Update tieba_page.js

--- scripts/tieba_page.js
+++ scripts/tieba_page.js
@@ -2,9 +2,6 @@ var body = $response.body;
 var json = JSON.parse(body);
 
 // 过滤掉特定关键词的对象
-if (json.hasOwnProperty("recom_ala_info")) {
-delete json["recom_ala_info"];
-}
 if (json.hasOwnProperty("banner_list")) {
 delete json["banner_list"];
 }
