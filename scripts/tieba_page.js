4a79fa89028dbef78b9dc33c70a4dc45d63c0c6f
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-10 03:01:07 +0800

    Add files via upload

---
+++ scripts/tieba_page.js
@@ -0,0 +1,20 @@
+var body = $response.body;
+var json = JSON.parse(body);
+
+// 过滤掉特定关键词的对象
+if (json.hasOwnProperty("recom_ala_info")) {
+delete json["recom_ala_info"];
+}
+if (json.hasOwnProperty("banner_list")) {
+delete json["banner_list"];
+}
+
+// 过滤掉"user"中的"user_growth"
+if (json.hasOwnProperty("thread") && json["thread"].hasOwnProperty("thread_recommend_infos")) {
+delete json["thread"]["thread_recommend_infos"];
+}
+if (json.hasOwnProperty("forum") && json["forum"].hasOwnProperty("banner_list")) {
+delete json["thread"]["banner_list"];
+}
+
+$done({ body: JSON.stringify(json) });
