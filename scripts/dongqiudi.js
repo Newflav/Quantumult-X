494de3b6f261d5d1dff3f95fcb8510369c4e85c7
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-11 00:15:48 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var json = JSON.parse(body);
-if (json.hasOwnProperty("infos")) {
-delete json["infos"];
+if (json.hasOwnProperty("ad_content")) {
+delete json["ad_content"];
 }
 $done({ body: JSON.stringify(json) });
