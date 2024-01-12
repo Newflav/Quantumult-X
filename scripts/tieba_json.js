0118f1feb079f9337f31b44b8c2ad5a297d4ca47
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-13 05:24:17 +0800

    Update tieba_json.js

--- scripts/tieba_json.js
+++ scripts/tieba_json.js
@@ -41,8 +41,7 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
         console.log(`去除用户中心广告`);
     } else {
         console.log(`无需用户中心广告`);
-} else if (url.includes("c/u/user/profile")) {
-    console.log('贴吧-user');
+    }
     if (body.user?.user_growth?.length) {
         body.user.user_growth = {};
         console.log(`去除用户成长广告`);
