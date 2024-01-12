877242cf823dde77cb357c28a43af46d3a29a17c
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-13 07:05:56 +0800

    Update tieba_json.js

--- scripts/tieba_json.js
+++ scripts/tieba_json.js
@@ -39,11 +39,10 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
     if (body.banner?.length) {
         body.banner = [];
         console.log(`去除用户中心广告`);
-    } else {
-        console.log(`不去除用户中心广告`);
     }
     if (body.user?.user_growth) {
     delete body.user.user_growth;
+    console.log(`去除贴吧等级广告`);
     }
 } else if (url.includes('c/s/sync')) {
     // get post(贴吧使用了post)均可访问
