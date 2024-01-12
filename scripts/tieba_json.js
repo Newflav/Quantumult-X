aa7dcdad725a57afd07c72fc45fde4d011dba632
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-13 07:13:11 +0800

    Update tieba_json.js

--- scripts/tieba_json.js
+++ scripts/tieba_json.js
@@ -177,6 +177,7 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
     
     if (body.thread?.thread_recommend_infos) {
     delete body.thread.thread_recommend_infos;
+console.log(`去除进入话题提醒`);
     }
     
     if (body.post_list?.length) {
