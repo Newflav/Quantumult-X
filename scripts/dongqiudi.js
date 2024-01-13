ff319870738b45a64bc7a56371ad43573065879a
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 06:27:55 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,9 +2,9 @@ const notifyTitle = "贴吧json脚本错误";
 console.log(`json-2024.1.14`);
 
 let body = JSON.parse($response.body);
-    if (body.data.infos.ad_content) {
-        console.log(`去除进入小窗`);
-        body.data.infos.ad_content = {};
+    if (body.data?.infos?.ad_content) {
+    delete body.data.infos.ad_content;
+console.log(`去除进入话题提醒`);
     }
 
 body = JSON.stringify(body);
