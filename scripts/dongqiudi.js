f438c18c696d6751a385efc4409d76b6496d6fa3
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 06:16:28 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -4,7 +4,7 @@ console.log(`json-2024.1.14`);
 let body = JSON.parse($response.body);
     if (body.data.infos.ad_content) {
         console.log(`去除进入小窗`);
-        body.data.infos.ad_content = [];
+        body.data.infos.ad_content = {};
     }
 
 body = JSON.stringify(body);
