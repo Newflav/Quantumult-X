559f15c3fda2bd6ebc46b192656247651ffad21d
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 07:30:07 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,7 +2,7 @@ var body = $response.body;
 var json = JSON.parse(body);
 
 let body = JSON.parse($response.body);
-  if (body.data?.info?.ad_content) {
+  if (body.data?.infos?.ad_content) {
       body.data.infos.ad_content = null;
       console.log('去除横幅');
   }
