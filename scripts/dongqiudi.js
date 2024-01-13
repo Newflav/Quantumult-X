d2a1d2cb531b4a93ca10dc29e2cf279409197ad3
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 06:11:01 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,7 +1,7 @@
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-  if (obj?.data.infos.ad_content) {
+  if (obj.data?.infos.ad_content) {
     // 收货时寄快递享八折 享受条件苛刻 故移除
     delete obj.data.infos.ad_content;
   }
