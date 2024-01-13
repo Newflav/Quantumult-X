63e5690f5a63243128336dc4ec2f95e3c5caa325
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 05:10:43 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,12 +1,11 @@
-const url = $request.url;
 console.log(`json-2024.1.13`);
-
 let body = JSON.parse($response.body);
-if (body.data?.infos?.ad_content) {
-    delete body.data.infos.ad_content;
-console.log(`去除横幅广告`);
-    }
 
+    if (body.data.infos.ad_content) {
+        console.log('去除横幅广告');
+        body.data.infos.ad_content = [];
+    }
+    
 body = JSON.stringify(body);
 
 $done({
