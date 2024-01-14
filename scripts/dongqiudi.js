f6ad509af20fb3ae4550d0172a9c847fc0614454
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 18:53:30 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -4,5 +4,6 @@ let body = JSON.parse($response.body);
         if (body.data.not_show_m_ad === "0") {
             body.data.not_show_m_ad = '1';
         }
+}
 
 $done({ body: JSON.stringify(body) });
