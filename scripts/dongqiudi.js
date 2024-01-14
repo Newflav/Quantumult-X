5cf1c5dba5417ed019600a4cd02e2d3f822135b6
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 08:11:38 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,6 +1,6 @@
 console.log(`贴吧json-2024.1.14`);
-
 let body = JSON.parse($response.body);
+
 if ('data' in body) {
         if (!body.data?.infos?.ad_content) {
             console.log('无需处理');
@@ -8,4 +8,8 @@ if ('data' in body) {
             console.log(`ad_str}`);
             body.ad_content = null;
 
-$done({ body: JSON.stringify(obj) });
+body = JSON.stringify(body);
+
+$done({
+    body
+});
