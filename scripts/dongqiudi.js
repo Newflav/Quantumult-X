def9e10fd871c58f72605b892e5cbe689f739053
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 08:02:58 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,11 +1,8 @@
-var body = $response.body;
-var json = JSON.parse(body);
-console.log(`json-2024.1.14`);
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
-let body = JSON.parse($response.body);
-  if (body.data?.infos?.ad_content) {
-      body.data.infos.ad_content = null;
-      console.log('去除横幅');
+  if (obj.data?.infos?.ad_content) {
+      obj.data.infos.ad_content = null;
   }
 
 $done({ body: JSON.stringify(json) });
