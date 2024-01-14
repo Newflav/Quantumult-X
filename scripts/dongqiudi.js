77852cc150dafc069ad32be580676628c0ce3a71
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 08:07:08 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,7 +2,7 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
   if (obj.data?.infos?.ad_content) {
-      detele.data.infos.ad_content;
+      detele.obj.data.infos.ad_content;
   }
 
 $done({ body: JSON.stringify(obj) });
