be715ba5f6580b511082a70c5701a2a15dc0424f
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-14 09:46:40 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -4,7 +4,6 @@ let body = JSON.parse($response.body);
     if ('data' in body) {
         if (body.data.not_show_m_ad === "0") {
             body.data.not_show_m_ad = '1';
-            console.log('不展示广告');
         }
 
 $done({ body: JSON.stringify(body) });
