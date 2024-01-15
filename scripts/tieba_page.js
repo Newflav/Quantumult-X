c7594714486d407cabece1c8c169f382af31cbf2
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-15 08:06:27 +0800

    Delete scripts/tieba_page.js

--- scripts/tieba_page.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-var json = JSON.parse(body);
-
-// 过滤掉"user"中的"user_growth"
-if (json.hasOwnProperty("thread") && json["thread"].hasOwnProperty("thread_recommend_infos")) {
-delete json["thread"]["thread_recommend_infos"];
-}
-
-$done({ body: JSON.stringify(json) });
