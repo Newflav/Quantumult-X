be70304fac4cba309c4b1d02bc0d35a67fe23f7c
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-15 20:36:18 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,17 +2,11 @@ if (!$response.body) $done({});
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
-  if (obj?.data?.infos) {
-    let list = obj.data.infos.ad_content;
-    // 详情页 底部 房产推广
-    if (list?.ad_source) {
-      let ad = list.ad_source;
-      if (ad?.image) {
-        delete ad.image;
-      }
+  if ("ad_content" in obj) {
+    delete obj;
+  }
 } else {
   $done({});
 }
 
 $done({ body: JSON.stringify(obj) });
-
