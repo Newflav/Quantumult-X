4e64165f55a371131e010b816650ad84c8950ceb
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-15 13:07:46 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,7 +2,7 @@ if (!$response.body) $done({});
 let body = JSON.parse($response.body);
     
     if (body.data?.infos) {
-        delete body.data?.infos;
+        delete body.data.infos;
     }
 } else {
   $done({});
