3ae2120ec7dd2080eb3ea824260c26bbde3ecccb
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-16 11:14:17 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,7 +1,10 @@
 if (!$response.body) $done({});
 const url = $request.url;
 let obj = JSON.parse($response.body);
-
+  
+  if (obj.data?.not_show_m_ad) {
+    obj.data.not_show_m_ad = null;
+  }
   if ("ad_content" in obj) {
     delete obj;
   }
