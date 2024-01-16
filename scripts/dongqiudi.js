504457f8197758bfb9848c1b4dd4db596627935a
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-16 15:46:42 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -3,9 +3,6 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
   
   if (url.includes("/v2/article/detail")) {
-    if (obj.data?.dqd_ads) {
-    delete obj.data.dqd_ads;
-  }
     if (obj.data?.not_show_m_ad) {
       obj.data.not_show_m_ad = 1;
   }
