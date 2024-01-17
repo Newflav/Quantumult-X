c3c6795f908b186f6240539bbefdb136a0285ac4
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-17 20:11:32 +0800

    Update bilibili.js

--- scripts/bilibili.js
+++ scripts/bilibili.js
@@ -16,7 +16,6 @@ if (url.includes("/x/resource/show/skin")) {
       (item) =>
         item.name === "直播" ||
         item.name === "推荐" ||
-        item.name === "热门" ||
         item.name === "番剧"
     );
     fixPos(obj.data.tab);
