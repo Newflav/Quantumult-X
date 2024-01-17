cd64c16d39abf0f611b9cbd5e7527f30355cd3da
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-17 10:07:53 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -2,8 +2,8 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-  if (obj?.data?.infos?.ad_content?.ad_resource?.length > 0) {
-    obj.data.infos.ad_content.ad_resource = [];
+  if (obj.data?.not_show_m_ad) {
+    obj.data.not_show_m_ad = 1;
   }
 } else {
   $done({});
