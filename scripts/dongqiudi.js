457ed3dcdbd3bcd2517ffd9bc38f530d113a6662
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-19 07:42:36 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,2 +1,3 @@
 let body=$response.body;if(body){switch(!0)
-{case/v2/article/detail/.test($request.url):try{let A=JSON.parse(body);A.data&&A.data.infos.ad_content&&(A.data.infos.ad_content=A.data.infos.ad_content.filter(t=>"banner"!=t.ad_type)),body=JSON.stringify(A)}catch(C){console.log("bilibili xlive:"+C)}break;default:$done({})}$done({body})}else $done({});
+{case/v2/article/detail/.test($request.url):try{let A=JSON.parse(body);A.data.infos&&A.data.infos.ad_content&&(A.data.infos.ad_content=A.data.infos.ad_content.filter(t=>"banner"!=t.ad_type)),body=JSON.stringify(A)}catch(C)
+break;default:$done({})}$done({body})}else $done({});
