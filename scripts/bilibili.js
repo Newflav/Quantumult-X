30470aa8ec93aa6575f1ec95e3f3a18156c5a62a
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-18 19:56:59 +0800

    Update bilibili.js

--- scripts/bilibili.js
+++ scripts/bilibili.js
@@ -16,9 +16,7 @@ if (url.includes("/x/resource/show/skin")) {
       (item) =>
         item.name === "直播" ||
         item.name === "推荐" ||
-        item.name === "热门" ||
-        item.name === "动画" ||
-        item.name === "影视" 
+        item.name === "热门" 
     );
     fixPos(obj.data.tab);
   }
