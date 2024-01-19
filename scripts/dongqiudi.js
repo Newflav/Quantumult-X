ff77a284b01fc9568b171b27e69e1c44f1817dcc
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-19 08:19:07 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -1,3 +1,9 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/boss/car/order/content_info")) {
   if (obj?.data?.infos?.ad_content?.length > 0) {
     obj.data.infos.ad_content = [];
   }
+}
