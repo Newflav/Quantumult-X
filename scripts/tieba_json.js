2fa98681dc6b0af9c15c1aea48237c8c39e7a6d3
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-20 07:22:43 +0800

    Update tieba_json.js

--- scripts/tieba_json.js
+++ scripts/tieba_json.js
@@ -174,10 +174,10 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
     } else {
         console.log('帖子详情页无直播广告');
     }
-  if (body.thread?.thread_recommend_infos) {
-    console.log(`去除进入话题小窗`);
-    delete body.thread.thread_recommend_infos;
-  }
+    if (body.thread.thread_recommend_infos) {
+     console.log(`去除进入话题小窗`);
+     body.thread.thread_recommend_infos = [];
+    }
   if (body.thread?.carTips?.data?.popupInfo) {
     delete body.data.carTips.data.popupInfo;
   }
