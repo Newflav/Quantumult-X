3879641be3386dc238a7089cbd9ba81d62d020b5
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-20 08:19:59 +0800

    Update tieba_json.js

--- scripts/tieba_json.js
+++ scripts/tieba_json.js
@@ -166,7 +166,7 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
 } else if (url.includes("c/f/frs/threadlist")) {
     console.log('贴吧-threadlist');
     removeGoodsInfo(body.banner_list?.app);
-} else if (url.includes("pb/page")) {
+} else if (url.includes("c/f/pb/page")) {
     console.log('贴吧-PbPage');
     if (body.recom_ala_info?.live_id) {
         console.log('帖子详情页推荐的直播广告去除');
@@ -174,11 +174,6 @@ if (url.includes("tiebaads/commonbatch") && method === postMethod) {
     } else {
         console.log('帖子详情页无直播广告');
     }
-    
-    if (body.thread.thread_recommend_infos) {
-        console.log(`去除进入话题小窗`);
-        body.thread.thread_recommend_infos = [];
-    } 
     
     if (body.post_list?.length) {
         for (const post of body.post_list) {
