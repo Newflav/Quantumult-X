d51f4cfa7f0a95a7ffcc01705ca74745b8bb3ba0
Newflav <149877783+Newflav@users.noreply.github.com>
2024-01-22 22:16:08 +0800

    Add files via upload

---
+++ scripts/zhonggong.js
@@ -0,0 +1,13 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/visitor/home/list")) {
+  if (obj.data?.floats) {
+    delete obj.data.floats;
+  }
+} else {
+  $done({});
+}
+
+$done({ body: JSON.stringify(obj) });
