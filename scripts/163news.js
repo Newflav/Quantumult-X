df8326502ede3242227c139e634b057310529109
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-03 22:09:10 +0800

    Update 163news.js

--- scripts/163news.js
+++ scripts/163news.js
@@ -2,17 +2,7 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/feed/dynamic/headline-list")) {
-  if (obj?.data?.items?.length > 0) {
-    // 移除置顶新闻
-    obj.data.items = obj.data.items.filter((i) => !(i?.tagList?.length > 0));
-  }
-} else if (url.includes("/search/hot-word")) {
-  if (obj?.data?.special?.length > 0) {
-    // 猜你想搜
-    obj.data.special = [];
-  }
-} else if (url.includes("/gentie-web/api/v3/products")) {
+if (url.includes("/gentie-web/api/v3/products")) {
   if (obj?.data?.secretaryVO) {
     // 跟帖小秘书
     delete obj.data.secretaryVO;
