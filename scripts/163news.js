0718fb5258fb1ca5a9407071ab90846659885842
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-04 02:55:26 +0800

    更新 163news.js

--- scripts/163news.js
+++ scripts/163news.js
@@ -8,11 +8,9 @@ if (url.includes("/gentie-web/api/v3/products")) {
     delete obj.data.secretaryVO;
   }
 } else if (url.includes("/feed/dynamic/video-normal-list")) {
+  if (obj?.data?.items?.videobanner) {
     // 视频页顶部横幅
-  if (obj?.data?.items?.videobanner?.length > 0) {
-    obj.data.items.videobanner = obj.data.items.videobanner.filter(
-      (i) => !["春节活动"]?.includes(i?.title)
-    );
+    delete obj.data.items.videobanner;
   }
 } else {
   $done({});
