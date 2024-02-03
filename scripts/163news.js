7637436afb5855cd9c3bea5bdb8131f6e8a03f02
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-03 22:48:24 +0800

    Update 163news.js

--- scripts/163news.js
+++ scripts/163news.js
@@ -9,7 +9,7 @@ if (url.includes("/gentie-web/api/v3/products")) {
   }
 } else if (url.includes("/feed/dynamic/video-normal-list")) {
   // 视频页顶部横幅
-  if (obj?.data?.items?.videobanner) {
+  if (obj.data?.items?.videobanner) {
     delete obj.data.items.videobanner;
   }
 } else {
