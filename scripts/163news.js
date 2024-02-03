8ad1c3f59f4bccafcf21fe41c6509f608542c69e
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-03 17:13:51 +0800

    Update 163news.js

--- scripts/163news.js
+++ scripts/163news.js
@@ -17,6 +17,11 @@ if (url.includes("/feed/dynamic/headline-list")) {
     // 跟帖小秘书
     delete obj.data.secretaryVO;
   }
+} else if (url.includes("/feed/dynamic/video-normal-list")) {
+  if (obj?.data?.items?.videobanner) {
+    // 视频页横幅
+    delete obj.data.items.videobanner;
+  }
 } else {
   $done({});
 }
