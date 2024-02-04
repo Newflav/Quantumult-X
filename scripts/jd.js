21ecc3685ba671cb152f5817a66f8503ec2f6f5b
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-05 07:03:16 +0800

    Update jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -131,6 +131,9 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
       (i) => !["float", "photoCeiling", "ruleFloat", "searchIcon", "topRotate"]?.includes(i?.type)
     );
   }
+if (obj?.floorList?.content?.subFloors?.data?.groupInfoList?.length > 0) {
+    obj.floorList.content.subFloors.data.groupInfoList = [];
+  }
   // 首页 顶部背景图
   if (obj?.topBgImgBig) {
     delete obj.topBgImgBig;
