a93a14328eacb1fd27ef55d12889bb983b315ade
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-06 01:20:21 +0800

    Update jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -123,8 +123,8 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
     obj.showTimesDaily = 0;
   }
 } else if (url.includes("functionId=welcomeHome")) {
-  if (obj?.floorList?.content?.subFloors) {
-    delete obj.floorList.content.subFloors;
+  if (obj?.floorList?.content?.subFloors?.length > 0) {
+    obj.floorList.content.subFloors = [];
   }
   // 首页配置
   if (obj?.floorList?.length > 0) {
