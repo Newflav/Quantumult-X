d444cff3726bbcbecca66984b21e45a66908884d
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-08 07:54:20 +0800

    Update i4.js

--- scripts/i4.js
+++ scripts/i4.js
@@ -2,13 +2,13 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-  if (url.includes("/getHotSearchList")) {
+if (url.includes("/getHotSearchList")) {
   // 热搜页面
-    if (obj?.spappli?.length > 0) {
-      obj.spappli = [];
+  if (obj?.spappli?.length > 0) {
+    obj.spappli = [];
   }
-    if (obj?.adli?.length > 0) {
-      obj.adli = [];
+  if (obj?.adli?.length > 0) {
+    obj.adli = [];
   }
 } else if (url.includes("/adclickcb")) {
     obj = [];
