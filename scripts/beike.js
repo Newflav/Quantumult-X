dcf167e36c858c56616b96bfc8ea4ad781b6f1e5
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-08 06:42:06 +0800

    Update beike.js

--- scripts/beike.js
+++ scripts/beike.js
@@ -9,11 +9,7 @@ if (url.includes("/config/ershoufang/content")) {
   }
 } else if (url.includes("/user/profile/getusercenterinfov4")) {
     //我的页面净化
-  const items = [
-    "banners",
-    "promises",
-    "myhouse"
-  ];
+  const items = ["banners","promises","myhouse"];
   if (obj?.data) {
     for (let i of items) {
       delete obj.data[i]
