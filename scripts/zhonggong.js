a3dc813a279bb65baffdfdbaf808927fabcf932f
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-10 04:31:01 +0800

    Update zhonggong.js

--- scripts/zhonggong.js
+++ scripts/zhonggong.js
@@ -6,9 +6,6 @@ if (url.includes("/visitor/home/list")) {
   if (obj.data?.floats) {
     delete obj.data.floats;
   }
-if (obj?.data?.list?.detail?.more_hidden?.length > 0) {
-    delete obj.data.list.detail.more_hidden
-  }
 }
 
 $done({ body: JSON.stringify(obj) });
