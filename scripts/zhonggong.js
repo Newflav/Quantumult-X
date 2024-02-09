f6ee2411e961efe846a19b06e67072249ab5f356
Newflav <149877783+Newflav@users.noreply.github.com>
2024-02-10 04:27:14 +0800

    Update zhonggong.js

--- scripts/zhonggong.js
+++ scripts/zhonggong.js
@@ -6,10 +6,8 @@ if (url.includes("/visitor/home/list")) {
   if (obj.data?.floats) {
     delete obj.data.floats;
   }
-if (obj?.data?.list?.detail?.length > 0) {
-   obj.data.list.detail = obj.data.list.detail.filter(
-      (i) => !["轮播多图"]?.includes(i?.title)
-    );
+if (obj?.data?.list?.detail?.more_hidden?.length > 0) {
+    delete obj.data.list.detail.more_hidden;
   }
 }
 
