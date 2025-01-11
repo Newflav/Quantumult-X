971aeae5d6ea42e74c8c01953bc5fb9423529a44
Newflav <149877783+Newflav@users.noreply.github.com>
2025-01-11 12:24:41 +0800

    Add files via upload

---
+++ scripts/Honda.js
@@ -0,0 +1,12 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("pageByTypeNewV2")) {
+  if (obj?.data) {
+    delete obj.data;
+  }
+
+}
+
+$done({ body: JSON.stringify(obj) });
