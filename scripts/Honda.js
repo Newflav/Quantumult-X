5e36dc68f0a2283f48b5743332768263c739a96f
Newflav <149877783+Newflav@users.noreply.github.com>
2025-01-21 20:52:21 +0800

    更新 Honda.js

--- scripts/Honda.js
+++ scripts/Honda.js
@@ -6,7 +6,11 @@ if (url.includes("pageByTypeNewV2")) {
   if (obj?.data) {
     delete obj.data;
   }
-
+}
+if (url.includes("startFind")) {
+  if (obj?.data) {
+    delete obj.data;
+  }
 }
 
 $done({ body: JSON.stringify(obj) });
