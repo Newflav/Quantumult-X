c04317ce1b91d5206dda9bd7b546be977b5d6819
Newflav <149877783+Newflav@users.noreply.github.com>
2025-01-26 21:56:12 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -289,6 +289,9 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
    if (obj?.wait) {
      obj.wait = 0;
   }
+   if (obj?.code) {
+     obj.code = 0;
+  }
 }
 
 $done({ body: JSON.stringify(obj) });
