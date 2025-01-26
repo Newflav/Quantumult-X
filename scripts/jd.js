61c69524834e481ac5f89010e5b15eb16b99b377
Newflav <149877783+Newflav@users.noreply.github.com>
2025-01-26 23:42:21 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -287,7 +287,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
   }
 } else if (url.includes("functionId=cart") || url.includes("functionId=subCartCount")) {
     if (obj?.code) {
-      obj.code = 0;
+      obj.code = 1;
   }
     if (obj?.wait) {
       obj.wait = 0;
