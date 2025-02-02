bd4bded4d0c9bfaef2fb8d679158d066ab276db4
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-02 20:47:35 +0800

    更新 honda.js

--- scripts/honda.js
+++ scripts/honda.js
@@ -2,7 +2,7 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("startFind")) {
+if (url.includes("ad/startFindstartFind")) {
   if (obj?.data) {
     delete obj.data;
   }
