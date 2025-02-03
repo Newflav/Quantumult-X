194a74b07eec925c5b59e75d7f6c3b49ab4339b8
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-03 22:49:58 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -47,48 +47,6 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
     obj.floors = newFloors;
   }
 } else if (url.includes("functionId=personinfoBusiness")) {
-  // 个人页面
-  if (obj?.floors?.length > 0) {
-    let newFloors = [];
-    for (let floor of obj.floors) {
-      // orderIdFloor我的订单 keyToolsFloor浏览记录 newWalletIdFloor我的钱包 iconToolFloor底部工具栏
-      const items = [
-        "bigSaleFloor", // 双十一
-        "newsFloor", //京东快讯
-        "buyOften", // 常买常逛
-        "newAttentionCard", // 关注的频道
-        "newBigSaleFloor", // 双十一
-        "noticeFloor", // 顶部横幅
-        "recommendfloor" // 我的推荐
-      ];
-      if (items?.includes(floor?.mId)) {
-        continue;
-      } else {
-          if (floor?.data?.commonPopup) {
-            delete floor.data.commonPopup;
-          }
-          // 弹窗
-          if (floor?.data?.commonPopup_dynamic) {
-            delete floor.data.commonPopup_dynamic;
-          }
-          // 底部会员续费横幅
-          if (floor?.data?.commonTips?.length > 0) {
-            floor.data.commonTips = [];
-          }
-          // 弹窗
-          if (floor?.data?.commonWindows?.length > 0) {
-            floor.data.commonWindows = [];
-          }
-          // 右下角动图
-          if (floor?.data?.floatLayer) {
-            delete floor.data.floatLayer;
-          }
-        } else if (floor?.mId === "orderIdFloor") {
-          if (floor?.data?.commentRemindInfo?.infos?.length > 0) {
-            // 发布评价的提醒
-    floor.data.commentRemindInfo.infos = [];
-          }
-        } else if (floor?.mId === "userinfo") {
           // 个人页 顶部背景图
           if (floor?.data?.bgImgInfo?.bgImg) {
             delete floor.data.bgImgInfo.bgImg;
@@ -97,12 +55,6 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
           if (floor?.data?.newPlusBlackCard) {
             delete floor.data.newPlusBlackCard;
           }
-        }
-        newFloors.push(floor);
-      }
-    }
-    obj.floors = newFloors;
-  }
 } else if (url.includes("functionId=start")) {
   // 开屏广告
   if (obj?.images?.length > 0) {
