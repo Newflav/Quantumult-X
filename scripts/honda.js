2fd20243d83dd1f3dcbdc768217de9ca415b3bde
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-04 16:12:14 +0800

    更新 honda.js

--- scripts/honda.js
+++ scripts/honda.js
@@ -2,11 +2,7 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("startFindstartFind") || url.includes("query")) {
-    if (obj?.data?.length > 0) {
-    obj.data = [];
-  }
-} else if (url.includes("pageByTypeNewV2")) {
+if (url.includes("startFindstartFind") || url.includes("query") || url.includes("pageByTypeNewV2")) {
     if (obj?.data) {
     delete obj.data;
   }
