e183e17733e701a7482acdf23c50950da1ce2191
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-04 15:13:17 +0800

    更新 honda.js

--- scripts/honda.js
+++ scripts/honda.js
@@ -2,15 +2,7 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("ad/startFindstartFind")) {
-    if (obj?.data?.length > 0) {
-    obj.data = [];
-  }
-} else if (url.includes("/pageByTypeNewV2")) {
-    if (obj?.data?.length > 0) {
-    obj.data = [];
-  }
-} else if (url.includes("/query")) {
+if (url.includes("startFindstartFind") || url.includes("pageByTypeNewV2") || url.includes("query")) {
   //开屏广告
     if (obj?.data?.length > 0) {
     obj.data = [];
