f3870ab2fa4bb93db443915dc2cb79239da1dd2a
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-04 20:57:20 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -126,7 +126,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
             let node = floor.data.nodes;
             if (node?.[0]?.length > 0) {
               // 第一组十个
-              node[0] = node[0]
+              node[1] = node[0]
                 .filter((i) => sortLists?.includes(i?.functionId))
                 .sort((a, b) => sortLists.indexOf(a?.functionId) - sortLists.indexOf(b?.functionId));
             }
