e06a1e71a9475a8ba09ba0238bc0e743dff494d5
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-07 00:32:33 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -112,6 +112,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
           // 底部工具栏
           if (floor?.data?.nodes?.length > 0) {
             const sortLists = [
+              "applezhushou", // apple助手 1-1-1
               "lingjindouxin", // 签到领豆 1-1-2
               "dongdongnongchangxin", // 京东农场 1-1-3
               "chongwangwang", // 宠汪汪 1-1-4
@@ -131,7 +132,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
             }
             if (node?.[1]?.length > 0) {
               // 第二组四个
-              node[0] = node[0]
+              node[1] = node[1]
                 .filter((i) => sortLists?.includes(i?.functionId))
                 .sort((a, b) => sortLists.indexOf(a?.functionId) - sortLists.indexOf(b?.functionId));
             }
@@ -139,7 +140,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
         } else if (floor?.mId === "orderIdFloor") {
           if (floor?.data?.commentRemindInfo?.infos?.length > 0) {
             // 发布评价的提醒
-    floor.data.commentRemindInfo.infos = [];
+            floor.data.commentRemindInfo.infos = [];
           }
         } else if (floor?.mId === "userinfo") {
           // 个人页 顶部背景图
@@ -188,19 +189,6 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
   if (obj?.webViewFloorList?.length > 0) {
     obj.webViewFloorList = [];
   }
-} else if (url.includes("functionId=cart")) {
-    if (obj?.config?.textInfo) {
-    delete obj.config.textInfo;
-  }
-      if (obj?.config?.jingEggStrongDateConfig) {
-    delete obj.config.jingEggStrongDateConfig;
-  }
-    if (obj?.config?.jingEggDateConfig) {
-    delete obj.config.jingEggDateConfig;
-  }
-    if (obj?.config?.jingEggCouponNumConfig) {
-    delete obj.config.jingEggCouponNumConfig;
-   }
 }
 
 $done({ body: JSON.stringify(obj) });
