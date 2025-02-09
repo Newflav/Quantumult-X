2c6fdd9138fb53531094f29fd2b544c296830559
Newflav <149877783+Newflav@users.noreply.github.com>
2025-02-09 17:23:32 +0800

    更新 jd.js

--- scripts/jd.js
+++ scripts/jd.js
@@ -138,6 +138,19 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
   if (obj?.webViewFloorList?.length > 0) {
     obj.webViewFloorList = [];
   }
+} else if (url.includes("functionId=cart")) {
+    if (obj?.config?.textInfo) {
+    delete obj.config.textInfo;
+  }
+    if (obj?.config?.jingEggStrongDateConfig) {
+    delete obj.config.jingEggStrongDateConfig;
+  }
+    if (obj?.config?.jingEggDateConfig) {
+    delete obj.config.jingEggDateConfig;
+  }
+    if (obj?.config?.jingEggCouponNumConfig) {
+    delete obj.config.jingEggCouponNumConfig;
+  }
 }
 
 $done({ body: JSON.stringify(obj) });
