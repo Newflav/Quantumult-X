a81741718d570348d47bf3e6854bea4231f80b7a
Newflav <149877783+Newflav@users.noreply.github.com>
2025-04-13 23:33:47 +0800

    Update amap.js

--- scripts/amap.js
+++ scripts/amap.js
@@ -239,10 +239,10 @@ if (url.includes("/boss/car/order/content_info")) {
   }
 } else if (url.includes("/shield/search_business/process/middleLayer")) {
   // 搜索页列表净化
-  if (obj.city_list?.tip_list?.tip) {
+  if (obj?.city_list?.tip_list?.tip) {
     delete obj.city_list.tip_list.tip;
   }
-  if (obj.tip_list?.tip) {
+  if (obj?.tip_list?.tip) {
     delete obj.tip_list.tip;
   }
 } else if (url.includes("/shield/search_poi/homepage")) {
