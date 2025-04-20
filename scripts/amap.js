0ade03010727d2de1123b6fd988c6a91ddfd926e
Newflav <149877783+Newflav@users.noreply.github.com>
2025-04-20 14:50:29 +0800

    Update amap.js

--- scripts/amap.js
+++ scripts/amap.js
@@ -220,18 +220,9 @@ if (url.includes("/boss/car/order/content_info")) {
     const items = ["comprehensiveEditEntranc", "check_in", "reviews", "merchantSettlement", "societyPublicExperience", "widgets", "business_scope", "business_scope", "comprehensiveEditEntrance", "claim", "evaluateVO", "discount_commodity", "yellowPageAdRecommendModule", "similarShopRecommend", "commonGoodsShelf", "platformCustomerCommonModule", "platformCustomerComplianceInfo", "subscription", "shopQualifications", "verification", "shopBasePerson", "brand_story", "push_data", "businessQualifications", "brand_service", "horizontalGoodsShelf", "CouponBanner", "poiDetailNewBeltV2", "human_traffic", "floorGuideGbf", "new_operation_banner", "commonAiAgent", "similarShelfRecommend", "travelGuideAndQa", "poiDetailWaterFeed", "poiDetailWaterFeedTitle", "dayTripRecommendList", "adStoreBigBannerModule", "outdoorActivity", "dayTripList", "scenic_ticket", "mini_hook_shelf", "scenic_filter", "packageShelf", "poiDetailCommonConfig", "serviceBookingGoodsShelf", "waterFallFeedTitle", "cityCardFeed", "hotelList", "flash_purchase_ticket", "cards", "hotelRooms", "roomSelect", "shop_story", "house_price_v2", "gas_station_recommend", "nearbyGoodCar", "shoppingMallEvent", "official_account", "houseShelf", "residentialOwners", "carServiceCard", "hotelMustRead", "hotelFacilities", "scenic_mustplay", "legal_document", "surroundOldSellHouse", "recommend_food", "surroundRentHouse", "rentSaleHouse", "serviceGuide", "anchor", "poiMapModule", "hkfMiniPortal", "transportation", "navbarMore", "enhanceCustomerServicePoiModule", "hotelGraphicDetail", "shopStructGift", "nearbyRecommendModule"];
     for (let i of items) {
       delete obj.data.modules[i];
-    }
- }
-  if (obj?.data) {
-    let list = obj.data.modules[0];
-    if (list?.attractGalleryInfo) {
-      let hookData = list.attractGalleryInfo.data;
-      if (hookData?.title) {
-        delete hookData.title;
-      }
-      if (hookData?.desc) {
-        delete hookData.desc;
-      }
+  }
+  if (obj?.data?.modules?.attractGalleryInfo?.data?.title) {
+    delete (obj.data.modules.attractGalleryInfo.data.title)
   }
  }
 } else if (url.includes("/shield/search/client/push")) {
