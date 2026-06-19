d629dbbf118ed3b1b8c5af7c990f265460340058
Newflav <149877783+Newflav@users.noreply.github.com>
2026-06-19 15:19:20 +0800

    Update dongqiudi.js

--- scripts/dongqiudi.js
+++ scripts/dongqiudi.js
@@ -26,15 +26,12 @@ obj.data.comment_list.filter(
     );
   }
   if (obj?.data?.recommend_list?.length > 0) {
-   obj.data.recommend_list = obj.data.recommend_list.filter(
-      (i) => !["12320463"]?.includes(i?.user_id)
-    );
-  }
-  if (obj?.data?.recommend_list?.length > 0) {
-   obj.data.recommend_list = obj.data.recommend_list.filter(
-      (i) => !["ad_dplus"]?.includes(i?.comment_type)
-    );
-  }
+  obj.data.recommend_list = obj.data.recommend_list.filter(
+    (i) =>
+      i?.user_id !== "12320463" &&
+      i?.comment_type !== "ad_dplus"
+  );
+}
 } else if (url.includes("/match/pre_analysis")) {
   // 直播页广告
   if (obj?.asian_plans) {
